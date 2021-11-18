5018ee7cd4e220164900025550ae80b437f53055
Juan <duran85juan@gmail.com>
2021-11-18 10:57:17 -0300

    trabajando con props basico

--- new_react_exe/counter_app_two/src/PrimeraApp.js
+++ new_react_exe/counter_app_two/src/PrimeraApp.js
@@ -4,7 +4,26 @@ import React from 'react';
 
 const PrimeraApp = () => {
 
-    return <h1>Hola Mundo...</h1>;
+    const saludo = 'Nuevo saludo desde variable';
+    const objetoPersona = {
+        nombre: 'Juan',
+        apellido: 'Perez',
+        edad: '30'
+    }
+    /* 
+        No se imprimen varibles booleanas, los arreglos los muestra concatenados como string 
+        Los objetos literales no se pueden mostrar directamente, se deben mostrar con JSON.stringify o utilizar el metodo Object.keys para mostrar un valor como estring o utilizar la etiqueta pre con el JSON.stringify para que se vea mejor
+    */
+
+    return (
+        <>
+            <h1>Hola Mundo...</h1>
+            <p>Este es el contenido de mi primera app en React</p>
+            <p>{saludo}</p>
+            <pre>{JSON.stringify(objetoPersona, null, 3)}</pre>
+            <p>{objetoPersona.nombre}</p>
+        </>
+    );
 }
 
 export default PrimeraApp;
