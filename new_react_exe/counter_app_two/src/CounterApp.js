ed8780dde7e5dfb1caf549dd2a71f0a67dddebd8
Juan <duran85juan@gmail.com>
2021-11-18 22:43:20 -0300

    trabajando con hooks

--- new_react_exe/counter_app_two/src/CounterApp.js
+++ new_react_exe/counter_app_two/src/CounterApp.js
@@ -1,17 +1,29 @@
 import { PropTypes } from 'prop-types';
-import { React } from 'react';
+import { React, useState } from 'react';
 
 const CounterApp = ({valor}) => {
 
-    const clickBotton = (e) => {
+    const [counter, setCounter] = useState(valor);
+
+    const clickButtonPlus = (e) => {
         console.log('click', e);
+        setCounter(counter + 1);
+        // o setCounter((c)=>c+1)
     }
+
+    const clickButtonMinus = () => {
+        if (counter > 0) setCounter(counter - 1);
+    }
+
+    const clickButtonReset = () => setCounter(0);
 
     return (
         <>
             <h1>Counter App</h1>
-            <h2>{valor}</h2>
-            <button onClick={clickBotton}>+1</button>
+            <h2>{counter}</h2>
+            <button onClick={clickButtonPlus}>+1</button>
+            <button onClick={clickButtonReset}>Reset</button>
+            <button onClick={clickButtonMinus}>-1</button>
         </>
     )
 }
