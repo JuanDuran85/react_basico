e974ffb0c0604e565e7158ed4be5d030a3ef9e2a
Juan <duran85juan@gmail.com>
2021-11-21 19:27:16 -0300

    cambio apikey

--- new_react_exe/gif_app_two/src/helpers/getGifs.js
+++ new_react_exe/gif_app_two/src/helpers/getGifs.js
@@ -1,6 +1,6 @@
 export const getGifsImg = async (categoria) => {
 
-    const urlApi = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(categoria)}&limit=10&api_key=Uy4j1W8gbn1xrQ6tqhHqBHt7a6vAMOAu`;
+    const urlApi = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(categoria)}&limit=10&api_key=jdQUvRTPGmQypr9iugDdZ8lfOCzFxYFc`;
     const response = await fetch(urlApi);
     const { data } = await response.json();
     
