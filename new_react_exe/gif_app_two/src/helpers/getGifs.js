b2ece4a72bf915704e7df8e906ff9c445bd56f00
Juan <duran85juan@gmail.com>
2021-11-21 19:22:50 -0300

    cambiando apikey publica gratis

--- new_react_exe/gif_app_two/src/helpers/getGifs.js
+++ new_react_exe/gif_app_two/src/helpers/getGifs.js
@@ -1,6 +1,6 @@
 export const getGifsImg = async (categoria) => {
 
-    const urlApi = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(categoria)}&limit=10&api_key=l0ZTnQsbxo0D7tMMbezTo5Jmv2peA6gZ`;
+    const urlApi = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(categoria)}&limit=10&api_key=Uy4j1W8gbn1xrQ6tqhHqBHt7a6vAMOAu`;
     const response = await fetch(urlApi);
     const { data } = await response.json();
     
