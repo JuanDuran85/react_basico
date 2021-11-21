22195bf8caa3d7ca91cf4a3bcdc82ef9097be156
Juan <duran85juan@gmail.com>
2021-11-20 23:49:32 -0300

    gifapptwo creando componentes

---
+++ new_react_exe/git_app_two/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import { Gifexpertapp } from './GifExpertApp';
+
+
+ReactDOM.render(
+  <Gifexpertapp />,
+  document.getElementById('root')
+);
