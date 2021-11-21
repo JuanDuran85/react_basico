c6fec902c3b27ebe2ca17aab0cfa3192503d207f
Juan <duran85juan@gmail.com>
2021-11-20 23:06:28 -0300

    testing unitarios

--- new_react_exe/counter_app_two/src/test/base/CounterApp.test.js
+++ new_react_exe/counter_app_two/src/test/base/CounterApp.test.js
@@ -1,20 +1,55 @@
-import '@testing-library/jest-dom';
-import { render } from '@testing-library/react';
-import { shallow } from 'enzyme';
-import React from 'react';
+import "@testing-library/jest-dom";
+import { render } from "@testing-library/react";
+import { shallow } from "enzyme";
+import React from "react";
 
-import '../setupTests'
-import CounterApp from '../../CounterApp';
+import "../setupTests";
+import CounterApp from "../../CounterApp";
 
-describe('Test general', () => {
-    test('debe mostrar el mensaje de Counter App', async () => {
-        const wrapper = render(<CounterApp />);
-        expect(wrapper.getByText('Counter App')).toBeTruthy();
+describe("Test general texto plano", () => {
+  test("debe mostrar el mensaje de Counter App", async () => {
+    const wrapper = render(<CounterApp />);
+    expect(wrapper.getByText("Counter App")).toBeTruthy();
+  });
+
+  test("Debe mostrar la palabra Counter App con Enzyme", () => {
+    const wrapper = shallow(<CounterApp />);
+    expect(wrapper.find("h1").text()).toBe("Counter App");
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  test("Debe mostrar el valor del contador", () => {
+    const valor = 5;
+    const wrapper = shallow(<CounterApp valor={valor} />);
+    const textoParrafo = wrapper.find("h2").text().trim();
+    expect(textoParrafo).toBe(`${valor}`);
+  });
+});
+
+describe('Acciones y/o eventos', () => {
+    let wrapper = shallow(<CounterApp/>);
+
+    beforeEach(() => {
+        wrapper = shallow(<CounterApp valor = { 5 }/>);
+    });
+    
+    test('Debe incrementar el contador al hacer un click sobre el boton de +1 ', () => {
+        wrapper.find('button').at(0).simulate('click');
+        expect(wrapper.find('h2').text().trim()).toBe('6');
     });
 
-    test('Debe mostrar la palabra Counter App con enzyme', () => {
-        const wrapper = shallow(<CounterApp />);
-        expect(wrapper.find('h1').text()).toBe('Counter App');
-        expect(wrapper).toMatchSnapshot();
+    test('Debe decrementar en uno el contador a hacer click sobre el boton de -1',()=>{
+        wrapper.find('button').at(2).simulate('click');
+        expect(wrapper.find('h2').text().trim()).toBe('4');
+    });
+
+    test('debe colocar el valor por defecto del contador al hacer un click sobre el boton de rest', () => {
+        wrapper.find('button').at(0).simulate('click');
+        wrapper.find('button').at(0).simulate('click');
+        wrapper.find('button').at(0).simulate('click');
+        expect(wrapper.find('h2').text().trim()).toBe('8');
+        wrapper.find('button').at(1).simulate('click');
+        expect(wrapper.find('h2').text().trim()).toBe('0');
     });
 });
+
