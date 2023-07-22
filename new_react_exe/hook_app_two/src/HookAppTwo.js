94127595d23eb282acd0972faf4b5caf014db1bc
Juan <duran85juan@gmail.com>
2023-07-22 14:09:08 -0400

    working with react

---
+++ new_react_exe/hook_app_two/src/HookAppTwo.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const HookAppTwo = () => {
+    return (
+        <>
+            <h1>HookApp</h1>   
+        </>
+    )
+}
---
+++ new_react_exe/hook_app_two/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { HookAppTwo } from './HookAppTwo';
+
+ReactDOM.render(
+  <HookAppTwo />,
+  document.getElementById('root')
+);
